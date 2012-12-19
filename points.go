aeb3f17cb778ed0081c15a51fd69348355b25c9a
kortschak <dan.kortschak@adelaide.edu.au>
2012-12-20 09:01:26 +1030

    Split out Interface/Comparable implementation
    Refactor into separate file to allow easy duplication into another
    package if derivation is required, and to prove no implementation
    details are left in the kdtree routines.

---
+++ points.go
@@ -0,0 +1,93 @@
+// Copyright ©2012 Dan Kortschak <dan.kortschak@adelaide.edu.au>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package kdtree
+
+import (
+	"math"
+)
+
+var (
+	_ Interface  = Points{}
+	_ Comparable = Point{}
+)
+
+// Randoms is the maximum number of random values to sample for calculation of median of
+// random elements.
+var Randoms = 100
+
+// A Point represents a point in a k-d space that satisfies the Comparable interface.
+type Point []float64
+
+func (p Point) Clone() Comparable                   { return append(Point(nil), p...) }
+func (p Point) Compare(c Comparable, d Dim) float64 { q := c.(Point); return p[d] - q[d] }
+func (p Point) Dims() int                           { return len(p) }
+func (p Point) Distance(c Comparable) float64 {
+	q := c.(Point)
+	var sum float64
+	for dim, c := range p {
+		d := c - q[dim]
+		sum += d * d
+	}
+	return sum
+}
+func (p Point) Extend(b *Bounding) *Bounding {
+	if b == nil {
+		b = &Bounding{p.Clone(), p.Clone()}
+	}
+	min := b[0].(Point)
+	max := b[1].(Point)
+	for d, v := range p {
+		min[d] = math.Min(min[d], v)
+		max[d] = math.Max(max[d], v)
+	}
+	*b = Bounding{min, max}
+	return b
+}
+
+// A Points is a collection of point values that satisfies the Interface.
+type Points []Point
+
+func (p Points) Bounds() *Bounding {
+	if p.Len() == 0 {
+		return nil
+	}
+	min := append(Point(nil), p[0]...)
+	max := append(Point(nil), p[0]...)
+	for _, e := range p[1:] {
+		for d, v := range e {
+			min[d] = math.Min(min[d], v)
+			max[d] = math.Max(max[d], v)
+		}
+	}
+	return &Bounding{min, max}
+}
+func (p Points) Index(i int) Comparable         { return p[i] }
+func (p Points) Len() int                       { return len(p) }
+func (p Points) Pivot(d Dim) int                { return Plane{Points: p, Dim: d}.Pivot() }
+func (p Points) Slice(start, end int) Interface { return p[start:end] }
+
+// A Plane is a wrapping type that allows a Points type be pivoted on a dimension.
+type Plane struct {
+	Dim
+	Points
+}
+
+func (p Plane) Less(i, j int) bool              { return p.Points[i][p.Dim] < p.Points[j][p.Dim] }
+func (p Plane) Pivot() int                      { return Partition(p, MedianOfRandoms(p, Randoms)) }
+func (p Plane) Slice(start, end int) SortSlicer { p.Points = p.Points[start:end]; return p }
+func (p Plane) Swap(i, j int) {
+	p.Points[i], p.Points[j] = p.Points[j], p.Points[i]
+}
